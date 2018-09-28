d041c99c3581ba4f9bb2033c082fd9372742ce60
Joshua Leonard <joshuadleonard@gmail.com>
2018-09-28 12:18:02 -0400

    comments added

---
+++ api/models/Comment.js
@@ -0,0 +1,22 @@
+/**
+ * Comments.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    text: {
+      type: Sequelize.STRING,
+      required: true,
+    },
+    
+  },
+  options: {
+
+  }
+
+};
+
